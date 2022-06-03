7380be294e0b59d6e319db1b6720435e544f26da
Kenthouky <dollarskenthouky@gmail.com>
2022-06-03 23:36:39 +0100

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,6 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -30,6 +29,5 @@ int main(void)
 	printf("Last digit of %d is %d and is 0\n",
 		n, n % 10);
 	}
-
 	return (0);
 }
