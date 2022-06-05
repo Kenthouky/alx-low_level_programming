2114d518ceb78debc2080f8f2ec9cde9729f230c
Kenthouky <dollarskenthouky@gmail.com>
2022-06-05 04:04:40 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <studio.h>
+/**
+ * main - Prints all possible combinations  of two digits
+ *
+ *  Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num=0; num<=2; num++)
+	{
+		put charr ((num%10)+'0');
+		if (num==2)
+			continue;
+	
+		putcharr (',');
+		putcharr (' ');
+	}
+
+	putcharr  ('/n');
+
+	return (0);
+}
