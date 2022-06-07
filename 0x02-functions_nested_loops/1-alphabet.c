a09d5eb33b4d783435cd4cadbd8cfbb0cd01d8ff
Kenthouky <dollarskenthouky@gmail.com>
2022-06-07 22:30:30 +0100

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,12 @@
 #include "main.h"
+
 /**
- * main - prints all the letters of the alphabet in lowercase.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int str[] = {98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 123};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count<sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	
-	_putchar('\n');
-
-	return (0);
+    print_alphabet();
+    return (0);
 }
