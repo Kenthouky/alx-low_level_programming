8e923dc74fbf7389f0708e020eceee670e894ed6
Kenthouky <dollarskenthouky@gmail.com>
2022-06-07 22:24:21 +0100

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,8 +9,8 @@ int main(void)
 	int str[] = {98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 123};
 	int count, sz;
 
-	sz = sizeof(dtr) / sizeof(int);
-	for (count = 0; count<sz; coun++)
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count<sz; count++)
 	{
 		_putchar(str[count]);
 	}
