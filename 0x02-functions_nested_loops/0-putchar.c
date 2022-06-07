9b413426050f2216e3dd0641af453790be8b1c08
Kenthouky <dollarskenthouky@gmail.com>
2022-06-07 20:05:52 +0100

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdin.h>
+/**
+ * main - prints _putchar
+ *
+ * Return 0
+ */
+int main()
+{
+	while (charred = '_putchar'; charred != '\0')
+		putchar(charred);
+	charred++;
+
+    return (0);
+}
