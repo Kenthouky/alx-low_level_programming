b4face4a962e9a636c82b8363e611fabd0a799ad
Kenthouky <dollarskenthouky@gmail.com>
2022-06-09 19:02:51 +0100

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests function which prints if integer is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int t;
+
+	t = 0;
+	positive_or_negative(t);
+
+	return (0);
+}
