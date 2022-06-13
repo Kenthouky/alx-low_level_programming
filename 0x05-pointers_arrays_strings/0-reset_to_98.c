ea21d8a0525d439799b337726c6b5eb5df2f13b9
Kenthouky <dollarskenthouky@gmail.com>
2022-06-13 13:44:53 +0100

    Reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,17 +7,17 @@
  * Return: Always 0.
  */
 
-void reset_to_98()
+void reset_to_98(int n, n*)
 {
-	int n, *ip_n;
+	int n, *ip_n*;
 
 	n = 402;
 	putchar(n);
 
-	ip_n = &n;
+	ip_n* = &n;
 
-	ip_n = 98;
-	putchar(ip_n);
+	ip_n* = 98;
+	putchar(ip_n*);
 
 	putchar("/n");
 	return (0);
