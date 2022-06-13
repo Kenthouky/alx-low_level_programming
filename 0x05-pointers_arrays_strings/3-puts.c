51107383c538e5d3834c428d6e51467170381ed4
Kenthouky <dollarskenthouky@gmail.com>
2022-06-13 20:48:37 +0100

    Put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_str - print a string followed by a nee line to stdout
+ * @str: string to be printed
+ *
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	int count = 0;
+
+	while count >=0
+	{
+		if str[count] == '\0'
+		{
+			putchar('\n');
+			break;
+		}
+		putchar(str[count]);
+		count++;
+	}
+}
