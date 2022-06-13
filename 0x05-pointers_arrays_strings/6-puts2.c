6a9027ac956246eb0d352e7bc910798f8e6d7406
Kenthouky <dollarskenthouky@gmail.com>
2022-06-13 21:35:53 +0100

    Pits 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,8 +3,7 @@
 /**
  * puts2 : prints every other character
  * of a string, starting with the first
- * character, followed by a new line
- * @str: string used
+ * @str: input string
  *
  * Return: Always 0
  */
