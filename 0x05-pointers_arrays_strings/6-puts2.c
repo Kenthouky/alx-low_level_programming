4ac2788f233955d73dd2d25d1695c425a565520e
Kenthouky <dollarskenthouky@gmail.com>
2022-06-13 21:29:14 +0100

    Puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts2 : prints every other character of a string, starting with the first character, followed by a new line
+ * @str: string used
+ *
+ * Return: Always 0
+ */
+
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
