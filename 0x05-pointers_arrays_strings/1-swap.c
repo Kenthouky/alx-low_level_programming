b0f1377a74ecf297baadaf32fb6c4731aff95bc6
Kenthouky <dollarskenthouky@gmail.com>
2022-06-13 14:33:30 +0100

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap - swaps the values of two integers
+ * @a: integer
+ * @b: integer
+ * Return: always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	a ^= b;
+	b ^= a;
+}
