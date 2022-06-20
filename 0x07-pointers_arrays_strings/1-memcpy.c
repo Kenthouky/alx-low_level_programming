1e057b6886bbd8115a2b0ed77e6093c46c80c61d
Kenthouky <dollarskenthouky@gmail.com>
2022-06-20 16:19:34 +0100

    Copy to memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**_memcpy - copies n from source to destination
+ * @n: bytes to be copied
+ * @src: sourcs
+ * @dest: destination
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0, i < n, i++)
+	{
+		(src + 1) = (dest + 1);
+	}
+
+	return (dest);
+}
