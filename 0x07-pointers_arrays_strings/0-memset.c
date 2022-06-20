6124e00fa422f0f4443f6107d8fe93553d573d47
Kenthouky <dollarskenthouky@gmail.com>
2022-06-20 16:27:20 +0100

    ha

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,14 +11,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0;
-	
-	while (i < n)
+	for (i = 0, i < n, i++)
 	{
-		i++;
+		*(s + i) = b;
 	}
-	
-	*(s + i) = b;
 
 	return (s);
 }
