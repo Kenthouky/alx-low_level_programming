3876603265d032ed9f8353a38d0d1f4819f1958a
Kenthouky <dollarskenthouky@gmail.com>
2022-06-21 10:01:43 +0100

    Stretch!

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,6 +5,7 @@
  * @s: string
  * Return: 
  */
+
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
@@ -14,5 +15,6 @@ char *_strchr(char *s, char c)
 			return (s + i);
 	if (*(s + i) == c)
 		return (s + i);
+	
 	return ('\0');
 }
