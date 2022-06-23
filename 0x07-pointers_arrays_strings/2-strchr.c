50f77c12ac525e57e6e120fb7f8e6ab17c853218
Kenthouky <dollarskenthouky@gmail.com>
2022-06-23 12:04:24 +0100

    Stretcher

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string
- * @c: character
- * @s: string*
- * Return: pointer to first occurence of c
+ * _strchr - locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
  */
-
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
@@ -16,6 +15,5 @@ char *_strchr(char *s, char c)
 			return (s + i);
 	if (*(s + i) == c)
 		return (s + i);
-	
 	return ('\0');
 }
