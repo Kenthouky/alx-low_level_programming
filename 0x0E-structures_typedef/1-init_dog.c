7b2efaf597af90f8ea91f2cb26c25976f9a50105
Kenthouky <dollarskenthouky@gmail.com>
2022-07-04 21:29:58 +0100

    gragra

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,13 +2,13 @@
 #include "dog.h"
 
 /*
- * init_dog - initializes variable type struct dog
+ * init_dog - initializes a variable of type struct dog
  *  @d: struct dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: Nothing
+ * Return: no return
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
